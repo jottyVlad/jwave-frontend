5e5bd5c4f8d59bd0dd331047f9cd5551427da289
jotty <bard143games@gmail.com>
2022-01-19 00:24:08 +0300

    added mediaSession using

--- js/song_name_update.js
+++ js/song_name_update.js
@@ -17,8 +17,18 @@ const set_song_name = () => {
     const new_song = xslRef.getElementsByClassName("streamstats")[5].textContent;
 
     if(current_song !== new_song) {
-        document.getElementById("now-playing").textContent =
-            xslRef.getElementsByClassName("streamstats")[5].textContent;
+        document.getElementById("now-playing").textContent = new_song;
+
+        if('mediaSession' in navigator) {
+            let splitted_name = new_song.split(" - ")
+            let title = splitted_name[1]
+            let artist = splitted_name[0]
+
+            navigator.mediaSession.metadata = new MediaMetadata({
+                title: title,
+                artist: artist
+            })
+        }
         current_song = new_song;
     }
 }
